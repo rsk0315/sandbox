5f1f622cc57b55168f417df489fe748e2f371208
rsk0315 <rsk0315@icloud.com>
2015-11-17 21:02:19 +0900

    Update foo.rb

--- foo.rb
+++ foo.rb
@@ -6,3 +6,7 @@ for while until do break redo next in return
 true false nil self __FILE__ __LINE__
 and or not
 class def alias undef module defined? super yield
+=begin
+Is this commented out.
+=end ddd
+ddd
