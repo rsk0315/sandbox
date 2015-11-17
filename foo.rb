d77175602de73641b0b35c3e05c74f8a4db09ca0
rsk0315 <rsk0315@icloud.com>
2015-11-17 22:30:22 +0900

    Update foo.rb

--- foo.rb
+++ foo.rb
@@ -5,7 +5,7 @@ if elsif else unless case when then begin end rescue retry ensure
 for while until do break redo next in return
 true false nil self __FILE__ __LINE__
 and or not
-class def alias undef module defined? super yield
+class foo def bar alias undef module defined? super yield
 =begin
 Is this commented out.
 =en =end ddd
@@ -16,7 +16,7 @@ Begin
 TRUE
 true
 True
-
+Proc a
 "##{0}12"
 "\#{0}12"
 "\\#{0}12"
