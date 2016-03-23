46aba3fb1ec4862799fb7e2aa3d44d2bf1196d91
suzune <rsk0315@icloud.com>
2016-03-23 21:21:13 +0900

    Update re.py

--- re.py
+++ re.py
@@ -6,5 +6,5 @@
   r'grault{,4}'
   r'\\\.\^\$\*\+\?\{\x00\u0000\U00000000\x[]'
   r'|(?iLmsuxc)(?P<NAME>...)(>P=NAME)(?#COMMENT)'
-  r'(?=)(?!)(?<=)(?<!)(?(id/name)yes|no)\A\Z\b\B\g<1>\2\D\w'
+  r'(?=a)(?!b)(?<=c)(?<!d)(?(id/name)yes|no)\A\Z\b\B\g<1>\2\D\w'
 )
