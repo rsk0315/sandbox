eef6ebdfc09e872fe17c3515cddaddaf830e8706
suzune <rsk0315@icloud.com>
2016-03-26 18:31:13 +0900

    Update re.py

--- re.py
+++ re.py
@@ -7,6 +7,6 @@
   r'\\\.\^\$\*\+\?\{\x00\u0000\U00000000\x[]'
   ur'\x0\x00\u0000\U00000000\N{LATIN SMALL LETTER A}'
   r'|(?iLmsuxc)(?P<NAME>...)(?P=NAME)(?#COMMENT)'
-  r'(?=a)(?!b)(?<=c)(?<!d)(?(id)yes|no)\A\Z\b\B\g<1>\2\D\w'
+  r'(?=a)(?!b)(?<=c)(?<!d)(?(id)yes|no)\A\Z\b\B\g<1>\2\D\w\x\y\z'
 )
 '\x00\u0000\U00000000'
